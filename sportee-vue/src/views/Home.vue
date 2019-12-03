0287253baed635d587d42561ce1d09e10a82dc9e
Katka <blatna@ulovdomov.cz>
2019-12-03 16:18:05 +0100

    Vue.js file added, files moved here, fontawesome npm install

---
+++ sportee-vue/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+	<div id="cover-photo">
+		<div class="text-cover-photo">
+			<h1>Sportuj s námi</h1>
+			<p>Sportuješ často a nebaví tě pořád sledovat rozvrhy jednotlivých sportovišť? Vyzkoušej naši aplikaci a
+				hned zjistíš, kde se co cvičí, a jestli je ještě volné místo.</p>
+		</div>
+	</div>
+
+<activityform />
+
+<signpost />
+
+
+</div>
+
+</template>
+
+<script>
+
+import ActivityForm from './../components/ActivityForm.vue'
+import SignPost from './../components/SignPost.vue'
+
+export default {
+  components: {
+    "activityform": ActivityForm,
+    "signpost": SignPost
+  }
+}
+
+</script>
+
+
+
+
+<style scoped>
+#cover-photo {
+  height: 500px;
+  background-image: url(../assets/gym.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.text-cover-photo {
+  padding: 30px;
+  padding-top: 150px;
+  color: white;
+}
+
+@media (min-width: 650px) {
+
+  .text-cover-photo {
+    padding-left: 20%;
+    padding-right: 20%;
+  }
+}
+</style>
---
+++ sportee-vue/src/views/Project.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Project">
+    <h1>This is project page</h1>
+  </div>
+</template>
