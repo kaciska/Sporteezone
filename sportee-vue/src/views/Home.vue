602936feffed786cd72cdedd47d06f330e8279e5
Katka <blatna@ulovdomov.cz>
2019-12-03 16:37:34 +0100

    Favicon added, container corrections

--- sportee-vue/src/views/Home.vue
+++ sportee-vue/src/views/Home.vue
@@ -8,11 +8,13 @@
 		</div>
 	</div>
 
-<activityform />
+  <div class="container">
 
-<signpost />
+  <activityform />
 
+  <signpost />
 
+  </div>
 </div>
 
 </template>
@@ -54,6 +56,17 @@ export default {
   .text-cover-photo {
     padding-left: 20%;
     padding-right: 20%;
+  }
+}
+
+@media (min-width: 860px) {
+  .container {
+    width: 960px;
+    background-color: white;
+    margin: -80px auto 0 auto;
+    box-shadow: 0 0 8px 1px rgba(140, 138, 140, 1);
+    position: relative;
+    z-index: 1;
   }
 }
 </style>
